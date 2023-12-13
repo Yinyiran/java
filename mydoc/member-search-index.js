e9fade71aa1e56bbcb1ebf2e367ebb4f691dd0b3
yinyiran <Yinyiran@users.noreply.github.com>
2023-12-13 17:31:46 +0800

    mydoc

---
+++ mydoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"HellowWorld","l":"HellowWorld()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HellowWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
