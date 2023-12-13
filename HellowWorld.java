0c816e6715b64055d68cbd9641fdf7b70e8e9ae2
yinyiran <Yinyiran@users.noreply.github.com>
2023-12-13 17:32:08 +0800

    mian

---
+++ HellowWorld.java
@@ -0,0 +1,23 @@
+/**
+ * HellowWorld
+ */
+public class HellowWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
+
+/**
+ * InnerHellowWorld2fawefawe
+ * 
+ * @author yinyiran
+ * @version 1.0
+ */
+class InnerHellowWorld2 {
+  /** fawefawe */
+  public static void main(String[] args) {
+    System.out.println("InnerHellowWorld2");
+  }
+}
