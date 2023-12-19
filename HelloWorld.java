fc119153d9c961c708269a69caeaff07b01d8f64
chrisgad <chrisgad@qq.com>
2023-12-19 23:26:47 +0800

    hello

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println("Hello World!");
+        System.out.println(x);
+        x = 200;
+        System.out.println(x);
+    }
+
+}
